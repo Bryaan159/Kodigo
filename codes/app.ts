2a5b10f1894d19f9ff32e74f4fb6d029fcd904f0
Bryaan159 <00057720@uca.edu.sv>
2023-07-28 13:38:21 -0600

    I added more code for learning more

--- codes/app.ts
+++ codes/app.ts
@@ -1,13 +1,24 @@
-// main.js
-let numberOfCards = 1;
-console.log("Hola mundo desde TypeScript!!");
-add();
-add();
-add();
-add();
-
-// Funciones
-function add() {
-    console.log('Linea #', numberOfCards);
-    numberOfCards+=1;
+console.log("object");
+let _1nombre = "Juan";
+let apellido = "Perez";
+console.log(_1nombre + " " + apellido);
+//Strings
+let firstName: string = 'Bryan';
+let lastName: string = 'Hernández';
+let letter = 0;
+for(let i = 0; i <= firstName.length; i++){
+    letter = i;
 }
+let fullName:string = firstName  + " " +lastName;
+console.log(fullName);
+console.log("Cantidad: "+letter);
+//----------
+let number1:number = 10;
+let number2:number = 20;
+console.log(number1 + number2);
+//-----------------------
+let isActive: boolean = true;
+let isRunning: boolean = false;
+let hasKids: boolean = true;
+//------------------------
+console.log('isActive: '+isActive);
