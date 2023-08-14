3e7143b353f175784d3257f0551f3d38b25f71d0
Bryaan159 <00057720@uca.edu.sv>
2023-08-14 00:43:11 -0600

    I finished my all homeworks ;v

---
+++ codes/homeworks/homeW4.ts
@@ -0,0 +1,5 @@
+import { getHighestNumber } from "../homeworks/functions";
+
+let numbers = [1,6,8,4,2,7,10,3,5];
+let highest = getHighestNumber(numbers);
+console.log("The highest number is: " + highest);
---
+++ codes/homeworks/homework1.ts
@@ -0,0 +1,4 @@
+import { convertPtoKg } from "../homeworks/functions";
+let pounds = 150;
+let kilograms = convertPtoKg(pounds);
+console.log(`${pounds} pounds is ${kilograms} kilograms`);
